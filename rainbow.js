e36ac36f9af8480eaad885b2d1f9187d2c54e3f7
Fyerfox <fyerfox12@gmail.com>
2017-10-21 01:23:05 -0500

    Cleaned up formatting

--- rainbow.js
+++ rainbow.js
@@ -1,38 +1,32 @@
+var frequency = .3;
 var linkCount=0;
-function changeColorOfLinks(color){
+var i=0;
+
+function changeColorOfLinks(color) {
   var links=document.getElementsByTagName("a");
   links[linkCount].style.color = color;
   linkCount++;
-  if(linkCount>=links.length){
+  if(linkCount>=links.length)
     linkCount=0;
-  }
-  // for(var i=0;i<links.length;i++){
-  //     links[i].style.color = color;
-  // }
 }
-function RGB2Color(r,g,b)
- {
+
+function RGB2Color(r,g,b) {
    return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
- }
-function byte2Hex(n)
-{
+}
+
+function byte2Hex(n) {
   var nybHexString = "0123456789ABCDEF";
   return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
 }
-var frequency = .3;
-var i=0;
-function colorShift(){
 
+function colorShift() {
      var red   = Math.sin(frequency*i + 0) * 127 + 128;
      var green = Math.sin(frequency*i + 2) * 127 + 128;
      var blue  = Math.sin(frequency*i + 4) * 127 + 128;
      ++i;
-     if(i>=1000000){
+     if(i>=1000000)
        i=0;
-     }
      changeColorOfLinks(RGB2Color(red,green,blue));
-    //  document.write( '<font color="' + RGB2Color(red,green,blue) + '">&#9608;</font>');
-
 }
 
 setInterval(function(){colorShift();}, 5);
